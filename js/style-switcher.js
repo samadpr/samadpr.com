fa2c782273165bb0ddde42b9137be4a9fb7cc877
Abdul Samad <codinguse341@gmail.com>
2024-10-11 07:59:36 +0530

    web site body full set

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -1,16 +1,18 @@
 // ============================ toggler style switcher ===============================
-document.addEventListener("DOMContentLoaded", () => {
-    const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
-    styleSwitcherToggle.addEventListener("click", () => {
-        document.querySelector(".style-switcher").classList.toggle("open");
-    });
-
-    window.addEventListener("scroll", () => {
-        if (document.querySelector(".style-switcher").classList.contains("open")) {
-            document.querySelector(".style-switcher").classList.remove("open");
-        }
-    });
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
 });
+// hide style - switcher on scroll
+window.addEventListener("scroll", () => {
+    if (document.querySelector(".style-switcher").classList.contains("open")) {
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
+});
+// document.addEventListener("DOMContentLoaded", () => {
+//     const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+    
+// });
 
 // ============================ theme colors ===============================
 const alternateStyles = document.querySelectorAll(".alternate-style");
@@ -28,3 +30,20 @@ function setActiveStyle(color)
     })
 }
 // ============================ theme light and dart mode ===============================
+
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark");
+})
+window.addEventListener("load", () => {
+    if(document.body.classList.contains("dark"))
+    {
+        dayNight.querySelector("i").classList.add("fa-sun");
+    }
+    else
+    {
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }
+})
